4acbb64b0c1392ee0a9ec74646e20d631ddb28b1
jjeong <jjeongwoo723@gmail.com>
2020-12-09 22:26:06 +0900

    Baekjoon1003_success
    C++ Vector class를 사용하여 실행함.
    Vector가 array보다 빠른듯함.

--- Baekjoon_test/problem_1003.cpp
+++ Baekjoon_test/problem_1003.cpp
@@ -1,45 +1,56 @@
-// Fail
+/*
+문제
+피보나치 함수에서 N이 주어졌을 때, fibonacci(N)을 호출했을 때, 0과 1이 각각 몇 번 출력되는지 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+각 테스트 케이스는 한 줄로 이루어져 있고, N이 주어진다. N은 40보다 작거나 같은 자연수 또는 0이다.
+
+출력
+각 테스트 케이스마다 0이 출력되는 횟수와 1이 출력되는 횟수를 공백으로 구분해서 출력한다.
+*/
+
+/*
+C++ Vector class를 사용하여 실행함.
+Vector가 array보다 빠른듯함.
+*/
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int Nzero;
+vector<int> Farray;
 
 int fibonacci(int n) 
 {
-    if (n == 0)
+    if (n <= Farray.size()-1) return Farray[n];
+    else
     {
-        Nzero++;
-        return 0;
-    } else if (n == 1)
-    {
-        return 1;
-    } else  return fibonacci(n-1) + fibonacci(n-2);
+        Farray.push_back(fibonacci(n-1) + fibonacci(n-2));
+        return Farray[n];
+    }
 }
 
 int main()
 {
-    int repeatN, Finput, None;
+    int repeatN, Finput, Fresult;
     
-    cin >> repeatN;
+    Farray.push_back(0);
+    Farray.push_back(1);
 
+    cin >> repeatN;
+    
     for(int i=0;i<repeatN;i++)  
     {
-        Nzero = 0;
         cin >> Finput;
-
-        if (Finput == 0)
-        {
-            Nzero = 1;
-            None  = 0;
-        } else
-        {
+        if(Finput==0)   cout << 1 <<" "<< 0 <<endl;
+        else
+        {            
             fibonacci(Finput);
-            None = Finput-Nzero;
+
+            cout << Farray[Finput-1] <<" "<< Farray[Finput] << endl;
         }
-        
-        cout << Nzero <<" "<< None << endl;
     }
 
     return 0;
